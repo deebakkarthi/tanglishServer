0d96f3b46bba493f789904ae7ce573ee7eaca5a9
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-11-30 06:21:36 +0530

    Add: N-Grams

--- tanglish.py
+++ tanglish.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 from flask import Flask, json, jsonify, request
-from nltk import word_tokenize, sent_tokenize
+from nltk import corpus, word_tokenize, sent_tokenize
+from nltk.lm.preprocessing import padded_everygram_pipeline
 import re
 import string
 
@@ -15,16 +16,38 @@ def remove_punctuation(str_: str):
     return str_.translate(str.maketrans("", "", string.punctuation))
 
 
-@app.route("/preprocess/", methods=["POST"])
-def handle_preprocess():
-    corpus = request.get_json()
-    corpus = corpus["text"]
-    corpus = sent_tokenize(corpus)
-    for ind, val in enumerate(corpus):
+def preprocess(str_):
+    str_ = sent_tokenize(str_)
+    for ind, val in enumerate(str_):
         tmp = conv_uchar_to_space(val)
         tmp = tmp.lower()
         tmp = re.sub(r"\d+", "", tmp)
         tmp = remove_punctuation(tmp)
         tmp = word_tokenize(tmp)
-        corpus[ind] = tmp
+        str_[ind] = tmp
+    return str_
+
+
+def ngram(str_, n):
+    str_ = preprocess(str_)
+    return padded_everygram_pipeline(n, str_)
+
+
+@app.route("/preprocess/", methods=["POST"])
+def handle_preprocess():
+    corpus = request.get_json()
+    corpus = corpus["text"]
+    corpus = preprocess(corpus)
     return jsonify(corpus)
+
+
+@app.route("/ngram/", methods=["POST"])
+def handle_ngram():
+    req = request.get_json()
+    corpus = req["text"]
+    n = req["n"]
+    train, vocab = ngram(corpus, n)
+    ret = {}
+    ret["train"] = [list(x) for x in train]
+    ret["vocab"] = [x for x in vocab]
+    return jsonify(ret)
