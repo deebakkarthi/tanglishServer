24967d3a8242836120ae29eeacb373306f828ebd
Deebakkarthi C R <dbk@deebakkarthi.com>
2023-12-06 19:33:07 +0530

    feat: Implemented saBERT

--- tanglish.py
+++ tanglish.py
@@ -1,18 +1,26 @@
 #!/usr/bin/env python3
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, Response
 from nltk import word_tokenize, sent_tokenize
 from nltk.lm.preprocessing import padded_everygram_pipeline
 from ai4bharat.transliteration import XlitEngine
+from keras_preprocessing.sequence import pad_sequences
+from keras.models import load_model
 import re
 import string
 import pickle
+from numpy import argmax
+import requests
+
+transliteration_model = XlitEngine("ta", beam_width=20, rescore=True)
+with open("./models/pos_crf_model.pkl", "rb") as f:
+    pos_model = pickle.load(f)
+with open("./models/ner_crf_model.pkl", "rb") as f:
+    ner_model = pickle.load(f)
+with open("./models/lstm_tokenizer.pkl", "rb") as f:
+    lstm_tokenizer = pickle.load(f)
+lstm_model = load_model("./models/llm_lstm_model.keras/")
 
 app = Flask(__name__)
-transliteration_model = XlitEngine("ta", beam_width=20, rescore=True)
-with open("./pos_crf_model.pkl", "rb") as f:
-    pos_model = pickle.load(f)
-with open("./ner_crf_model.pkl", "rb") as f:
-    ner_model = pickle.load(f)
 
 
 def conv_uchar_to_space(str_):
@@ -23,21 +31,22 @@ def remove_punctuation(str_: str):
     return str_.translate(str.maketrans("", "", string.punctuation))
 
 
-def preprocess(str_):
+def preprocess(str_, do_word_tokenize=True):
     str_ = sent_tokenize(str_)
     for ind, val in enumerate(str_):
         tmp = conv_uchar_to_space(val)
         tmp = tmp.lower()
         tmp = re.sub(r"\d+", "", tmp)
         tmp = remove_punctuation(tmp)
-        tmp = word_tokenize(tmp)
+        if do_word_tokenize:
+            tmp = word_tokenize(tmp)
         str_[ind] = tmp
     return str_
 
 
-def ngram(str_, n):
+def ngram(str_):
     str_ = preprocess(str_)
-    return padded_everygram_pipeline(n, str_)
+    return padded_everygram_pipeline(2, str_)
 
 
 def transliterate(str_):
@@ -119,6 +128,26 @@ def ner(corpus: str):
     return ner_model.predict(X)
 
 
+def transform_to_lstm_dataset(corpus: list):
+    corpus = lstm_tokenizer.texts_to_sequences(corpus)
+    corpus = pad_sequences(corpus)
+    return corpus
+
+
+def sa_lstm(corpus: list):
+    CLASSES = ["Mixed_feelings", "Negative", "Positive", "unknown_state"]
+    X = transform_to_lstm_dataset(corpus)
+    y_pred = lstm_model.predict(X)
+    return list(map(lambda x: CLASSES[argmax(x)], y_pred))
+
+
+def sa_bert(corpus: list):
+    API_URL = "http://127.0.0.1:42070/"
+    request_body = {"sentences": corpus}
+    res = requests.post(API_URL, json=request_body)
+    return res.json()
+
+
 @app.route("/preprocess/", methods=["POST"])
 def handle_preprocess():
     corpus = request.get_json()
@@ -131,8 +160,7 @@ def handle_preprocess():
 def handle_ngram():
     req = request.get_json()
     corpus = req["text"]
-    n = req["n"]
-    train, vocab = ngram(corpus, n)
+    train, vocab = ngram(corpus)
     ret = {}
     ret["train"] = [list(x) for x in train]
     ret["vocab"] = [x for x in vocab]
@@ -157,3 +185,17 @@ def handle_ner():
     corpus = request.get_json()["text"]
     corpus = transliterate(corpus)
     return jsonify(ner(corpus))
+
+
+@app.route("/saLSTM/", methods=["POST"])
+def handle_sa_lstm():
+    corpus = request.get_json()["text"]
+    corpus = preprocess(corpus, do_word_tokenize=False)
+    return jsonify(sa_lstm(corpus))
+
+
+@app.route("/saBERT/", methods=["POST"])
+def handle_sa_bert():
+    corpus = request.get_json()["text"]
+    corpus = preprocess(corpus, do_word_tokenize=False)
+    return jsonify(sa_bert(corpus))
